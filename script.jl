e05bf871c73fe388c5ed27ec4833b0f939e9caa6
Lucas Rodrigues Schiavetti <lurvetti@gmail.com>
2024-02-28 12:14:21 -0300

    functions

--- script.jl
+++ script.jl
@@ -1,5 +1,5 @@
 ### VARIABLES ###
-
+println("\n---- VARIABLES ----")
 # definição de variáveis
 name = "Lucas"
 age = 35
@@ -13,7 +13,7 @@ print('\n')
 
 
 ### STRUCTS ###
-
+println("\n---- STRUCTS ----")
 # structs (or composite types) - variáveis com relações
 struct Pessoa
   name::String
@@ -40,9 +40,9 @@ gabs_m.name_m = "Gabriel"
 
 println("Immutable - ", gabs)
 println("Mutable - ", gabs_m)
-print('\n')
 
 ### BOOLEAN OPERATORS ###
+println("\n---- BOOLEAN OPERATORS ----")
 println(false)
 println(!false)
 println(false | true)
@@ -53,3 +53,56 @@ println(10 == 10.0)
 println(32.40 isa Float64)
 
 ### FUNCTIONS ###
+println("\n---- FUNCTIONS ----")
+
+# normal
+function sum(a, b)
+  result = a + b
+  return result
+end
+println("sum: ", sum(10, 56))
+# result - variable within function is local - raises erros not defined
+
+# compact
+sub(c, d) = c - d
+println("sub: ", sub(10,9))
+
+# with type definition
+function mult(e, f::Float64)
+  return e * f
+end
+mult(2, 5.0)
+
+# with multiple methods
+function divv(g, h::Int64)
+  println("div with int")
+  return g / h
+end
+function divv(g, h::Float64)
+  println("div with float")
+  return g / h
+end
+println(methods(divv))
+divv(10, 2)
+divv(10, 3.0)
+
+# with abastract types
+function test_integer(x::Integer)
+  println("x is an Integer? ", x isa Integer)
+  return x
+end
+test_integer(1)
+
+
+### MULTIPLE DSPATCH ###
+println(("\n---- Multiple Dispatch ----"))
+
+struct Game
+  title::String
+  year_release::Int64
+end
+Base.show(io::IO, g::Game) = println("The game ", g.title, " was realeased in ", g.year_release)
+
+red_dead = Game("Red Dead Redemption", 2014)
+show(red_dead)
+show(gabs)
